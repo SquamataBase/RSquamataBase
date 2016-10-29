cf72a1782cde25ac33e29b646536d89716375e29
Michael Grundler <mcgrundler@gmail.com>
2016-10-28 21:49:27 -0400

    better

--- R/get_foodrecords.R
+++ R/get_foodrecords.R
@@ -23,6 +23,20 @@ get_foodrecords = function(predator=NULL, prey=NULL, detailed=FALSE) {
     } else {
         stop("No query parameters were provided.")
     }
-    foodrecords = jsonlite::fromJSON(query)
-    return(foodrecords[[1]])
+    foodrecords = jsonlite::fromJSON(query, flatten=TRUE)[[1]]
+    col_names = matrix(c(
+        "predator.id", "pred_id",
+        "prey.id", "prey_id",
+        "predator.taxon", "predator",
+        "prey.taxon", "prey",
+        "predator.count", "pred_amount",
+        "prey.count", "prey_amount",
+        "details.locality.coordinates.longitude", "longitude",
+        "details.locality.coordinates.latitude", "latitude",
+        "details.locality.country", "country",
+        "details.locality.state", "state", 
+        "details.locality.county", "county"), ncol = 2, byrow = TRUE)
+    foodrecords = foodrecords[,col_names[,1]]
+    colnames(foodrecords) = col_names[,2]
+    return(foodrecords)
 }
