da062d09a59749f8522efd27965b3f49dbd176e3
Michael Grundler <mcgrundler@gmail.com>
2017-05-31 10:03:59 -0400

    fix collapse_ranks function

--- R/collapse_ranks.R
+++ R/collapse_ranks.R
@@ -12,36 +12,30 @@ collapse_ranks = function(x, collapse, predator=TRUE, prey=FALSE) {
 
     if (predator) {
         hierarchy = get_taxonomy(taxon_id=unique(x$predator_taxon_id))
-        i = 1L
         for (h in hierarchy[[2]]) {
-            
+            matchedrows = which(x$predator_taxon == h$scientific_name[1])
             if (match(h$rank[1], levels(ranks)) <= collapse) {
-                matchedrows = which(x$predator_taxon == h$scientific_name[1])
                 x$predator_taxon[matchedrows] = h$scientific_name[match(levels(ranks)[collapse], h$rank)]
                 x$predator_taxon_id[matchedrows] = h$id[match(levels(ranks)[collapse], h$rank)]
                 x$predator_taxon_rank[matchedrows] = h$rank[match(levels(ranks)[collapse], h$rank)]
 
             } else {
-                keep[i] = FALSE
+                keep[matchedrows] = FALSE
             }
-            i = i + 1L
         }
     }
     if (prey) {
         hierarchy = get_taxonomy(taxon_id=unique(x$prey_taxon_id))
-        i = 1L
         for (h in hierarchy[[2]]) {
-            
+            matchedrows = which(x$prey_taxon == h$scientific_name[1])
             if (match(h$rank[1], levels(ranks)) <= collapse) {
-                matchedrows = which(x$prey_taxon == h$scientific_name[1])
                 x$prey_taxon[matchedrows] = h$scientific_name[match(levels(ranks)[collapse], h$rank)]
                 x$prey_taxon_id[matchedrows] = h$id[match(levels(ranks)[collapse], h$rank)]
                 x$prey_taxon_rank[matchedrows] = h$rank[match(levels(ranks)[collapse], h$rank)]
 
             } else {
-                keep[i] = FALSE
+                keep[matchedrows] = FALSE
             }
-            i = i + 1L
         }
     }
 
